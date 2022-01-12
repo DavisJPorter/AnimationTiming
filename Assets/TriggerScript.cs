9064259c1ad8fcc38bdb47109650b6f0b6c8d666
Jordan Davis <jordandavisporter@Hotmail.com>
2022-01-12 13:06:01 -0400

    Updating animations accordingly

---
+++ Assets/TriggerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TriggerScript : MonoBehaviour
+{
+    public Animation animation;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            animation.Play();
+
+        }
+    }
+
+
+
+
+}
